6b22af7ccac4184bc55084b6dc37793777a291a2
Elaine <elaine.silva@zup.com.br>
2021-09-23 13:20:32 -0300

    criando super classe inimigo

--- src/entities/classes/mae/Inimigos.java
+++ src/entities/classes/mae/Inimigos.java
@@ -1,4 +1,27 @@
 package entities.classes.mae;
 
 public class Inimigos {
+
+    private String nome;
+    private double vida;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void receberDano(double dano) {
+        vida -= dano;
+    }
+
+    public void exibirVida() {
+        System.out.println("|  A vida atual do inimigo é: " + vida);
+    }
 }
