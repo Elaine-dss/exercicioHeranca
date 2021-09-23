b77c9034f45eebb19c6849dd499d6035a807e770
Elaine <elaine.silva@zup.com.br>
2021-09-23 14:06:54 -0300

    adicionando funcionalidades da classe Voadores

--- src/entities/Voadores.java
+++ src/entities/Voadores.java
@@ -4,7 +4,22 @@ import entities.classes.mae.Inimigos;
 
 public class Voadores extends Inimigos {
 
-    public Voadores(String nome, double vida) {
+    private double danoTiro;
+
+    public Voadores(String nome, double vida, double danoTiro) {
         super(nome, vida);
+        this.danoTiro = danoTiro;
+    }
+
+    public double getDanoTiro() {
+        return danoTiro;
+    }
+
+    public void setDanoTiro(double danoTiro) {
+        this.danoTiro = danoTiro;
+    }
+
+    public double aplicarDano() {
+        return danoTiro;
     }
 }
